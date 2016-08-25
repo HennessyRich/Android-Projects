ee89f8641bb435ec34b6b01dc7795a342078f8e0
HennessyRich <denis.bondarenko@ukr.net>
2016-08-25 13:54:51 +0300

    75-78

---
+++ P0771_TabIntent/src/ru/startandroid/develop/p0771tabintent/TwoActivity.java
@@ -0,0 +1,15 @@
+package ru.startandroid.develop.p0771tabintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Денис on 25.08.2016.
+ */
+public class TwoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
